0a1f4100ac3ca1a95985c7d5e475794198839b95
Karrissa Volcy <v.karrissa1@gmail.com>
2023-02-03 11:05:58 -0500

    created reducer and wrapped the app with context
    created reducer and wrapped the app with global context

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,27 @@
-import '@/styles/globals.css'
+import "@/styles/globals.css";
+import { useReducer } from "react";
+import MovieContext from "../context/MovieContext";
+
+const initialState = {};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "ADD_MOVIE":
+      return {
+        ...state,
+        [action.payload.id]: action.payload.data,
+      };
+    default:
+      return state;
+  }
+};
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  return (
+    <MovieContext.Provider value={{ state, dispatch }}>
+      <Component {...pageProps} />
+    </MovieContext.Provider>
+  );
 }
