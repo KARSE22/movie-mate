a29fa74f8d4121773b899c6d3b29dfc3ee87737d
Karrissa Volcy <v.karrissa1@gmail.com>
2023-02-03 12:16:13 -0500

    handled above the folds image issue

--- components/MovieCard/MovieCard.jsx
+++ components/MovieCard/MovieCard.jsx
@@ -1,6 +1,7 @@
 import Link from 'next/link';
 import Image from "next/image";
 import styles from "./MovieCard.module.scss";
+const aboveTheFoldIds = ['tt0371746', 'tt1228705', 'tt2250912', 'tt2015381', 'tt0478970', 'tt0478970'];
 
 export default function MovieCard({ movie }) {
 
@@ -10,7 +11,8 @@ export default function MovieCard({ movie }) {
         <div className={styles.frame}>
           <div className={styles.card__image_container}>
             <figure>
-              <Image src={movie.Poster} alt={movie.Title} width={300} height={445} className={styles.thumbnail} />
+              <Image src={movie.Poster} alt={movie.Title} width={300} height={445} className={styles.thumbnail}
+              priority={ aboveTheFoldIds.includes(movie.imdbID) ? true : false} />
             </figure>
           </div>
           <div className={styles.card_caption}>
